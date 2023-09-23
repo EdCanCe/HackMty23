f6818715aa6ec6cad5ef645d56ea208f6a7eada4
Gcumplido59 <143289394+Gcumplido59@users.noreply.github.com>
2023-09-23 17:42:02 -0600

    image

--- front.py
+++ front.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title= "tak Coach de inversión", layout= "wide")
 
 with open("style.css") as f:
     st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
     st.title("Presentacion")
     st.write("BLa blablablalbala")
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
