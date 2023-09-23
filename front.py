cdbbced92961514ac6edf01d0e5c33b245bf890e
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 17:39:36 -0600

    image tec monterrey

--- front.py
+++ front.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title= "tak Coach de inversión", layout= "wide")    
 
 with open("style.css") as f:
-    st.header("Hola")
+    st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
     st.title("Presentacion")
     st.write("BLa blablablalbala")
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
