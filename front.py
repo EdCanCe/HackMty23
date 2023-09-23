6efaeaed2028ad6ae932f7d18351b5a40060b7a5
EdCanCe <edcance.dev@gmail.com>
2023-09-23 16:44:15 -0600

    a

--- front.py
+++ front.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-st.set_page_config(page_title= "Takatakata's Hackathon Proyect", layout= "wide")
-
-st.header("Hola")
-st.title("Presentacion")
-st.write("BLa blablablalbala")
+st.set_page_config(page_title= "Takatakata's Hackathon Proyect", layout= "wide")    
 
 with open("style.css") as f:
+    st.header("Hola")
+    st.title("Presentacion")
+    st.write("BLa blablablalbala")
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
     st.write("hello world".format(f.read()))
     st.write("bye world")
