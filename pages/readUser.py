036f60fafc7a57e8e7bb860e92da43f544c4ab4b
EdCanCe <edcance.dev@gmail.com>
2023-09-23 17:33:48 -0600

    a

--- pages/readUser.py
+++ pages/readUser.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(page_title= "Takatakata's Hackathon Proyect", layout= "wide")    
 
 with open("style.css") as f:
+    st.markdown("<style>.header{border: green;}</style>", unsafe_allow_html=True)
     st.header("Hola")
     st.title("Presentacion")
     st.write("BLa blablablalbala")
