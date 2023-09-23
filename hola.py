1375f739eb22ba4008e773dad48b2d3733c9a0db
EdCanCe <95944066+EdCanCe@users.noreply.github.com>
2023-09-23 11:24:46 -0600

    xd

--- hola.py
+++ hola.py
@@ -10,3 +10,17 @@ pipipi :c
 
 for i in range(10):
     st.write("am hola")
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
