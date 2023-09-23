9a39ff7ecbec6f730d235786b9bfb5f172966882
EdCanCe <95944066+EdCanCe@users.noreply.github.com>
2023-09-23 14:58:11 -0600

    a

--- datascrapping.py
+++ datascrapping.py
@@ -2,7 +2,7 @@ import requests
 
 ak = "SAJ8M4DTOL6CL1Q4"
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey='+ak
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey='+ak
 r = requests.get(url)
 data = r.json()
 
