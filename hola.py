2fa245f92921da8db2f70a5ed521a3afeca54c3f
EdCanCe <95944066+EdCanCe@users.noreply.github.com>
2023-09-23 11:10:34 -0600

    aaa

--- hola.py
+++ hola.py
@@ -1,1 +1,10 @@
-print("Hola mundo, wasaaaaaaaa")
+import streamlit as st
+import pandas as pd
+    
+st.write("""
+# My first app
+Hello *world!*
+""")
+    
+df = pd.read_csv("my_data.csv")
+st.line_chart(df)
