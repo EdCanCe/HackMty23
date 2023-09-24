11f070732c92c4068701e7844473a50ff2a8df9d
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 22:37:38 -0600

    as

--- preguntas.py
+++ preguntas.py
@@ -13,7 +13,7 @@ st.text_input("Dirección actual")
 st.number_input("¿Cuánto dinero ganas?")
 
 st.text("¿Su objetivo principal de inversión es a ?")
-st.radio("corto plazo (1-3 años)", "mediano plazo (3-5 años)" , "largo plazo (más de 5 años)")
+st.radio(["corto plazo (1-3 años)", "mediano plazo (3-5 años)" , "largo plazo (más de 5 años)"])
 
-st.text("¿Qué nivel de riesgo estás dispuesto a asumir en sus inversiones?")
+st.text(["¿Qué nivel de riesgo estás dispuesto a asumir en sus inversiones?"])
 st.radio("bajo", "moderado", "alto")
