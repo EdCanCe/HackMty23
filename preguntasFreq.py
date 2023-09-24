20932f46ca36d53b6818e368be1762109db33261
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-24 04:36:29 -0600

    sfsa

--- preguntasFreq.py
+++ preguntasFreq.py
@@ -4,13 +4,21 @@ st.set_page_config(page_title= "Preguntas frecuentes", layout= "wide")
 st.title('Preguntas frecuentes en Banorte')
 
 st.subheader("¿Qué es un fondo de inversión?")
+st.text_area("Fondos de Inversión", "Son una excelente forma para obtener rendimientos"
+            "de tus ahorros. Su funció principal es captar, invertir y administrar los recursos"
+            "de clientes como tú para invertirlos en distintos instrumentos financieros"
+            "y ponerlos a tu alcance. Funcionan como una sociedad que reúne el ahorro de varias"
+            "personas que también buscan invertir, pero que por los montos o instrumentos"
+            "no puede hacerlo de manera individual")
 
 st.subheader("¿Cómo puedo empezar a invertir en un fondo?")
+st.text_area("Invertir en un fondo", "Solo tienes que elegir el o los Fondos que te más te interesen"
+            "De acuerd a tu elección irás recibiendo rendimientos, que son resultado de la rentabilidad"
+            "o ganancia de los activos en los que invirtió el Fondo que elegiste"
+            "y ponerlos a tu alcance. Funcionan como una sociedad que reúne el ahorro de varias"
+            "personas que también buscan invertir, pero que por los montos o instrumentos"
+            "no puede hacerlo de manera individual")
 
 st.subheader("¿Por qué invertir en Banorte?")
+st.text_area("Banorte brinda", "Crecimiento", "Experiencia", "Diversificación", "Flexibilidad")
 
-# Crear un slider de texto
-valor_seleccionado = st.slider("Selecciona un valor:", ["Opción 1", "Opción 2", "Opción 3"])
-
-# Mostrar el valor seleccionado
-st.write("Has seleccionado:", valor_seleccionado)
