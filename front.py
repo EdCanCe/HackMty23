5d84c9b8d2dfdaf095cbbeece56dbc3599939e49
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 18:10:30 -0600

    no style

--- front.py
+++ front.py
@@ -2,12 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title= "tak Coach de inversión", layout= "wide")    
 
-with open("style.css") as f:
-    st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
-    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
-    st.image("https://th.bing.com/th/id/OIP.bD70SkJLNjIoODY7YK_VnAHaFE?pid=ImgDet&rs=1")
-    st.title("Presentacion")
-    st.write("BLa blablablalbala")
-    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-    st.write("hello world".format(f.read()))
-    st.write("bye ola")
+
+st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
+st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
+st.image("https://th.bing.com/th/id/OIP.bD70SkJLNjIoODY7YK_VnAHaFE?pid=ImgDet&rs=1")
+st.title("Presentacion")
+st.write("BLa blablablalbala")
+st.write("hello world")
+st.write("bye ola")
