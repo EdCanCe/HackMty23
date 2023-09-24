9ff68d7e5c43646e3df229b6c32a89e14b062a1b
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 18:47:46 -0600

    as

--- front.py
+++ front.py
@@ -8,14 +8,11 @@ with st.container():
         st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
     with image_HackMTY:     
         st.image("https://th.bing.com/th/id/OIP.bD70SkJLNjIoODY7YK_VnAHaFE?pid=ImgDet&rs=1")
-    text_column1, image_Banorte, text_column3  = st.columns((3))
+    text_column1, image_Banorte  = st.columns((1, 2))
     with text_column1:
         st.text("Caca") 
     with image_Banorte:
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
-    with text_column3:
-        st.text("Pedo")
-    
     st.title("Presentacion")
     st.write("BLa blablablalbala")
     st.write("hello world")
