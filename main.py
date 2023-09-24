1a996f8856abf9e5929dc51c486766e374de99c5
EdCanCe <edcance.dev@gmail.com>
2023-09-24 03:19:47 -0600

    a

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import varAux
+import DataBase as db
 st.set_page_config(
     page_title= "takatakata's Hackathon Proyect"
 )
@@ -119,10 +120,10 @@ if(st.session_state.page == 0): #Página de inicio
     with placeholder.container():
         genHeader()
         #Seccion Header 
+        redirect("Iniciar Sesión", 3, "hbis")
+        redirect("Crear Cuenta", 1, "hbcc")
         hpct=st.container()
         with hpct:
-            redirect("Iniciar Sesión", 3, "hbis")
-            redirect("Crear Cuenta", 4, "hbcc")
             st.markdown("---")
             with st.container():
                 image_Tec, image_HackMTY  = st.columns((7, 3))
@@ -151,14 +152,78 @@ if(st.session_state.page == 1): #Página de captura de
     st.session_state.page=varAux.auxPage
     with placeholder.container():
         genHeader()
-        st.markdown('''## Proporciona tus datos:
-    ''')
-        # cuanto ganas mensualmente
-        # 
-        #
-        #
-        #
-        #
+        st.subheader("Crear Cuenta")
+        Nombre = st.text_input("Nombre(s)") # Para insertar texto
+
+
+        Apellido = st.text_input("Apellido(s)")
+
+
+        N_oficial = st.text_input("Número de identificación oficial")
+
+
+        Direccion = st.text_input("Dirección actual")
+        st.write("Tu Dirección actual es: ", Direccion)
+
+        # Preguntas de opción múltiple
+
+        ObjetivoPrincipal = st.radio("¿Su objetivo principal de inversión es a ?", 
+                                    ["Corto plazo (1-3 años)", "Mediano plazo (3-5 años)" , "Largo plazo (más de 5 años)"], 
+                                    index = None, horizontal = True,
+        )
+
+
+        Riesgo = st.radio("¿Qué nivel de riesgo estás dispuesto a asumir en sus inversiones?",
+                        ["Bajo", "Moderado", "Alto"], 
+                        index = None, horizontal = True,
+        )
+
+
+        IngMensual = st.number_input("¿Cuánto es su ingreso mensual?")
+
+
+        InvMensual = st.number_input("¿Cuánto dinero planeas invertir mensualmente sin afectar tu calidad de vida actual?")
+
+
+        IngAnual = st.number_input("¿Cuánto es su ingreso anual?")
+
+
+        InvAnual = st.number_input("¿Cuánto dinero planeas invertir anualmente sin afectar tu calidad de vida actual?")
+
+
+        # Preguntas de varias opciones
+
+        metaInversion = st.multiselect("¿Cuáles son tus metas de inversión?", ["Casa", "Coche", "Retiro", "Educación de sus hijos", "Vacaciones", "Deudas", "Fondos de emergencia", "Otro"])
+
+        if "Otro" in metaInversion:
+            OtroMeta = st.text_input('Ingresa los otros')
+        else:
+            OtroMeta = ""   
+
+
+        # Compras
+
+        Necesidades = st.number_input("¿Cuánto gastas en necesidades (comida, servicios básico, agua, entre otros) al mes?")
+
+
+        Gustos = st.number_input("¿Cuánto gastas en gustos (compras de impulso, innecesarios, vicios, entre otros) al mes?")
+
+
+        if(st.button("Continuar")):
+            st.write(Nombre)
+            st.write(Apellido)
+            st.write(N_oficial)
+            st.write(ObjetivoPrincipal)
+            st.write(Riesgo)
+            st.write(IngMensual)
+            st.write(IngMensual)
+            st.write(IngAnual)
+            st.write(InvAnual)
+            st.write(metaInversion)
+            if "Otro" in metaInversion:
+                st.write(OtroMeta)
+            st.write(Necesidades)
+            st.write(Gustos)
         redirect("Volver", varAux.auxPage, "dtgb")
         genFooter()
 
@@ -167,12 +232,22 @@ if(st.session_state.page == 2): #Página de las FAQ
     with placeholder.container():
         genHeader()
         redirect("Volver", varAux.auxPage, "faqgb1")
-        st.markdown('''## Frecuently asked questions
-    ''')
+        
         redirect("Volver", varAux.auxPage, "faqgb2")
         genFooter()
 
+if(st.session_state.page == 3): #Página de inicio de sesión
+    st.session_state.page=varAux.auxPage
+    with placeholder.container():
+        genHeader()
+        st.subheader("Iniciar Sesión")
 
+        redirect("Iniciar sesión", 4, "isb")
+        genFooter()
 
-
-
+if(st.session_state.page == 2): #Página de usuario
+    st.session_state.page=varAux.auxPage
+    with placeholder.container():
+        genHeader()
+        st.subheader("Bienvenido usuario")
+        genFooter()
