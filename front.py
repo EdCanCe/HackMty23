7e8aa7e3a8d41c5a13a96a02dbdc83e857a527cf
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 18:31:40 -0600

    image column

--- front.py
+++ front.py
@@ -3,7 +3,9 @@ import streamlit as st
 st.set_page_config(page_title= "tak Coach de inversión", layout= "wide")    
 
 with st.container():
-    st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
+    image_column = st.columns((1,2))
+    with image_column: 
+        st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
     st.image("https://th.bing.com/th/id/OIP.bD70SkJLNjIoODY7YK_VnAHaFE?pid=ImgDet&rs=1")
     st.title("Presentacion")
