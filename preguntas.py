257da5262b530418b932ed6b14f4f83d5629913e
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-24 02:52:32 -0600

    asd

--- preguntas.py
+++ preguntas.py
@@ -69,7 +69,7 @@ if(st.button("Continuar")):
     st.write(IngAnual)
     st.write(InvAnual)
     st.write(metaInversion)
-    if "Otro" != "":
-        metaInversion = metaInversion + OtroMeta
+    if "Otro" in metaInversion:
+        st.write(OtroMeta)
     st.write(Necesidades)
     st.write(Gustos)
