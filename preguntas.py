3a60f683d4860457189a44c47dc2c9e7a72e75d5
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 22:30:56 -0600

    as

--- preguntas.py
+++ preguntas.py
@@ -2,22 +2,18 @@ import streamlit as st
 
 st.set_page_config(page_title= "Datos del usuario", layout= "wide")
 
-st.text("Nombre(s)")
-st.text_input("")
+st.text_input("Nombre(s)")
 
-st.text("Apellido(s)")
-st.text_input("a")
+st.text_input("Apellido(s)")
 
-st.text("Número de identificación oficial")
-st.text_input("b")
+st.text_input("Número de identificación oficial")
 
-st.text("Dirección actual")
-st.text_input("c")
+st.text_input("Dirección actual")
 
-st.text("¿Cuánto dinero ganas?")
-st.number_input("")
+st.number_input("¿Cuánto dinero ganas?")
 
 st.text("¿Su objetivo principal de inversión es a ?")
-st.selectbox("corto plazo (1-3 años), mediano plazo (3-5 años), largo plazo (más de 5 años)")
-st.text("¿Qué nivel de riesgo está dispuesto a asumir en sus inversiones?")
-st.selectbox("bajo, moderado, alto")
+st.selectbox("corto plazo (1-3 años)", "mediano plazo (3-5 años)" , "largo plazo (más de 5 años)")
+
+st.text("¿Qué nivel de riesgo estás dispuesto a asumir en sus inversiones?")
+st.selectbox("bajo", "moderado", "alto")
