385c1c4e32947ecd868f0e48b576e301ff02e5f6
Gcumplido59 <143289394+Gcumplido59@users.noreply.github.com>
2023-09-23 21:56:56 -0600

    ftgf

--- front.py
+++ front.py
@@ -30,7 +30,7 @@ with st.container():
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
     with text_column3:
         st.text("") 
-    sticker_lottie, text_column4, text_column5 = st.columns((3,1, 1))
+    sticker_lottie, text_column4, text_column5 = st.columns((2,1, 1))
     with sticker_lottie:
         st_lottie(lottie_coding, height = 300, key = "coding")
     with text_column4:
