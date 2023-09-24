c4eec4447b9a1578f8d1603cdb164aa17570cead
Gcumplido59 <143289394+Gcumplido59@users.noreply.github.com>
2023-09-23 18:57:58 -0600

    sticker

--- front.py
+++ front.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import requests
 
+from streamlit_lottie import st_lottie
 st.set_page_config(page_title= "tak Coach de inversión", layout= "wide")    
 
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+#------Agregar valores-------
+lottie_coding = load_lottieurl("https://lottie.host/60707005-ac51-4186-bf8d-ec558648f6e2/GUWk8gO9eR.json")
+
+
+#Seccion Header
 with st.container():
     image_Tec, image_HackMTY  = st.columns((2, 1))
     with image_Tec: 
@@ -29,3 +42,5 @@ with st.container():
 .......
 ......
 """)
+    with right_column:
+        st_lottie(lottie_coding, height 300, key "coding")
