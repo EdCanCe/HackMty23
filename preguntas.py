d3ca63097129bdaee056b87f11293bbcad6197fd
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 22:23:20 -0600

    pre

--- preguntas.py
+++ preguntas.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
 st.set_page_config(page_title= "Datos del usuario", layout= "wide")
-st.text("¿Cuánto dinero ganas wey?")
+
+st.text("Nombre(s)")
+st.text_input("")
+
+st.text("Apellido(s)")
+st.text_input("")
+
+st.text("Número de identificación oficial")
+st.text_input("")
+
+st.text("Dirección actual")
+st.text_input("")
+
+st.text("¿Cuánto dinero ganas?")
 st.number_input("")
+
+st.text("¿Su objetivo principal de inversión es a ?")
+st.selectbox("corto plazo (1-3 años), mediano plazo (3-5 años), largo plazo (más de 5 años)")
+st.text("¿Qué nivel de riesgo está dispuesto a asumir en sus inversiones?")
+st.selectbox("bajo, moderado, alto")
