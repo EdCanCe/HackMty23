4cae149cb03e3c3ae9f346b416160e6a3fce2c30
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-24 06:13:56 -0600

    ewr

--- preguntasFreq.py
+++ preguntasFreq.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title= "Preguntas frecuentes", layout= "wide")
 
-st.title('Preguntas frecuentes en Banorte')
+st.markdown(':red[Preguntas frecuentes en Banorte]')
 
 st.subheader("¿Qué es un fondo de inversión?")
 st.text_area("Fondos de Inversión","Son una excelente forma para obtener rendimientos "
