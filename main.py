8d2676ecf3bde67de730dae0534f48bfdf8bbb97
EdCanCe <edcance.dev@gmail.com>
2023-09-24 00:47:02 -0600

    a

--- main.py
+++ main.py
@@ -80,47 +80,34 @@ st.markdown(
 if(st.session_state.page == 0): #Página de inicio
     with placeholder.container():
         genHeader()
-        #Seccion Header
-        with st.container():
-            image_Tec, text_column2, image_HackMTY  = st.columns((3, 1 , 2))
-        with image_Tec: 
-            st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
-        with text_column2:
-            st.text("") 
-        with image_HackMTY:     
-            st.image("https://th.bing.com/th/id/OIP.bD70SkJLNjIoODY7YK_VnAHaFE?pid=ImgDet&rs=1")
-        text_column1, image_Banorte, text_column3  = st.columns((1, 3, 1))
-        with text_column1:
-            st.text("") 
-        with image_Banorte:
-            st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
-        with text_column3:
-            st.text("") 
-        sticker_lottie, text_column4, text_column5 = st.columns((2,1, 1))
-        with sticker_lottie:
-            st.write("---")
-            st_lottie(lottie_coding, height = 300, key = "coding")
-        with text_column4:
-            st.text("") 
-        with text_column5:
-            st.text("") 
-
-    with st.container():
-        st.write("---")
-        left_column, right_column = st.columns(2)
-        with left_column:
-            st.header("Que se hace")
-            st.write("##")
-            st.write(
-    """Hola, quienes somos, etc ,,,,,
-    .......
-    ......
-    """)
-
-
-
-        redirect("Pasar página", 1, "hba")
-        genFooter()
+        #Seccion Header 
+        hpct=st.container()
+        with hpct:
+            with st.container():
+                image_Tec, text_column2, image_HackMTY  = st.columns((3, 1 , 2))
+            with image_Tec: 
+                st.image("https://citris-uc.org/wp-content/uploads/2019/10/Tec-de-Monterrey-logo-horizontal-blue.png")
+            with text_column2:
+                st.text("") 
+            with image_HackMTY:     
+                st.image("https://th.bing.com/th/id/OIP.bD70SkJLNjIoODY7YK_VnAHaFE?pid=ImgDet&rs=1")
+            text_column1, image_Banorte, text_column3  = st.columns((1, 3, 1))
+            with text_column1:
+                st.text("") 
+            with image_Banorte:
+                st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Logo_de_Banorte.svg/2560px-Logo_de_Banorte.svg.png")
+            with text_column3:
+                st.text("") 
+            sticker_lottie, text_column4, text_column5 = st.columns((2,1, 1))
+            with sticker_lottie:
+                st.write("---")
+                st_lottie(lottie_coding, height = 300, key = "coding")
+            with text_column4:
+                st.text("") 
+            with text_column5:
+                st.text("") 
+            redirect("Pasar página", 1, "hba")
+            genFooter()
 
 if(st.session_state.page == 1): #Página de captura de 
     st.session_state.page=varAux.auxPage
