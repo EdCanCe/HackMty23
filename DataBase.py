d17f2cbd35334c95e982bd6e8837932cfde5dcc2
Gcumplido59 <143289394+Gcumplido59@users.noreply.github.com>
2023-09-24 03:07:55 -0600

    Data Base 1/2

--- DataBase.py
+++ DataBase.py
@@ -1,17 +1,29 @@
 import pandas as pd
 
-data = {'Nombre': [], 'Edad': [], 'Ciudad': []}
+data = {'IDINE': [], 'username': [], 'userlast': [], 'useradress': [], 'usertime': [], 'userisk': [], 'usermensual' : [], 'invemensual' : [], 'ingranual' : [], 'inveanual' : [], 'metainver' : [], 'otrameta' : [], 'necesidad' : [], 'gustos' : []}
 df = pd.DataFrame(data)
 
 def agregar_datos():
     global df
-    nombre = input("Ingresa el nombre: ")
-    edad = int(input("Ingresa la edad: "))
-    ciudad = input("Ingresa la ciudad: ")
+    IDINE = input("Ingresa el ID: ")
+    username = input("Ingresa el nombre: ")
+    userlast = input("Ingresa tus apellidos: ")
+    useradress = input("Ingresa tu dirección: ")
+    usertime = input("Tiempo de inversion: ")
+    userisk = input("Riesgo: ")
+    usermensual = float(input("ingreso mensual: "))
+    invemensual = float(input("cuanto dinero planeas invertir mensualmente: "))
+    ingranual = float(input("ingreso anual: "))
+    inveanual = float(input("cuanto dinero planeas invertir anualmente: "))
+    metainver = input("meta de inversion: ")
+    otrameta = input("otra meta: ")
+    necesidad = input("necesidades donde se va el dinero: ")
+    gustos = input("en que gustos gastas tu dinero: ")
 
-    nueva_fila = {'Nombre': nombre, 'Edad': edad, 'Ciudad': ciudad}
-    df = df.loc[0:]  # Añade esta línea para crear una copia del DataFrame
-    df += pd.DataFrame([nueva_fila], columns=df.columns)
+
+    nueva_fila = {'IDINE': IDINE, 'username': username, 'userlast': userlast, 'useradress': useradress, 'usertime': usertime, 'userisk': userisk, "usermensual" : usermensual, "invemensual" : invemensual, "ingranual" : ingranual, "inveanual" : inveanual, "metainver" : metainver, "otrameta" : otrameta, "necesidad" : necesidad, "gustos" : gustos  }
+    nueva_data = pd.DataFrame([nueva_fila])
+    df = pd.concat([df, nueva_data], ignore_index=True)
     return df
 
 while True:
@@ -24,41 +36,4 @@ while True:
         break
 
 df.to_csv('datos.csv', index=False)
-
-
-
-
-
-
-
-
-#d = pd.read_excel("New.xlsx")
-#df = pd.DataFrame(d)
-#print(df)
-
-
-
-
-"""IDINE = ["C13"]
-username = ["Mario"]
-lastname = ["Feng Wu"]
-useradress = ["Tec de Monterrey"]
-useropen = ["13,000"]
-usertime =["3"]
-userisk = ["Low"]
-
-df = pd.DataFrame()
-
-df["ID"] = IDINE
-df["Username"] =  username
-df["Lastname"] = lastname
-df["Useradress"] = useradress
-df["UserOpen"] = useropen
-df["UserTime"] = usertime
-df["Userisk"] = userisk
-    print(df)
-"""
 #df.to_excel("C:\\Users\\gaelm\\Desktop\\Data.xlsx")
-
-#with pd.ExcelWriter("Output.xlsx") as writer:
-#df.to_excel(writer, sheet_name= "Prueba")
