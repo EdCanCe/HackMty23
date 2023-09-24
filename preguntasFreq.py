cfd7feb88589619af466de8b93160ecc46f5db0b
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-24 04:46:11 -0600

    FAq

--- preguntasFreq.py
+++ preguntasFreq.py
@@ -5,7 +5,7 @@ st.title('Preguntas frecuentes en Banorte')
 
 st.subheader("¿Qué es un fondo de inversión?")
 st.text("Fondos de Inversión")
-st.text_area("Son una excelente forma para obtener rendimientos"
+st.text_area("","Son una excelente forma para obtener rendimientos"
             "de tus ahorros. Su funció principal es captar, invertir y administrar los recursos"
             "de clientes como tú para invertirlos en distintos instrumentos financieros"
             "y ponerlos a tu alcance. Funcionan como una sociedad que reúne el ahorro de varias"
@@ -14,7 +14,7 @@ st.text_area("Son una excelente forma para obtener rendimientos"
 
 st.subheader("¿Cómo puedo empezar a invertir en un fondo?")
 st.text("Invertir en un fondo")
-st.text_area("Solo tienes que elegir el o los Fondos que te más te interesen"
+st.text_area("","Solo tienes que elegir el o los Fondos que te más te interesen"
             "De acuerdo a tu elección irás recibiendo rendimientos, que son resultado de la rentabilidad"
             "o ganancia de los activos en los que invirtió el Fondo que elegiste"
             "y ponerlos a tu alcance. Funcionan como una sociedad que reúne el ahorro de varias"
@@ -23,11 +23,11 @@ st.text_area("Solo tienes que elegir el o los Fondos que te más te interesen"
 
 st.subheader("¿Por qué invertir en Banorte?")
 st.text("Banorte brinda")
-st.text_area("Crecimiento: Busca el incremento de tu patrimonio con la" 
+st.text_area("","Crecimiento: Busca el incremento de tu patrimonio con la" 
              "tranquilidad de que estás en buenas manos con una amplia oferta. ")
-st.text_area("Experiencia: Contarás con asesoría personalizada y recibirás toda" 
+st.text_area("","Experiencia: Contarás con asesoría personalizada y recibirás toda" 
              "la información necesaria para que puedas tomar la mejor decisión de inversión para ti.")
-st.text_area("Diversificación: Podrás diversificar tu dinero de acuerdo con la estrategia de cada Fondo,"
+st.text_area("","Diversificación: Podrás diversificar tu dinero de acuerdo con la estrategia de cada Fondo,"
              "así minimizas el riesgo e incertidumbre")
-st.text_area("Flexibilidad: Encontrarás diferentes opciones que se acomoden a tus necesidades y objetivos.")
+st.text_area("","Flexibilidad: Encontrarás diferentes opciones que se acomoden a tus necesidades y objetivos.")
 
