886da3d85e98bbfb8f8ec2c22bd4ecd3f1c903f1
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-24 02:21:57 -0600

    write

--- preguntas.py
+++ preguntas.py
@@ -2,13 +2,17 @@ import streamlit as st
 import DataBase as db
 st.set_page_config(page_title= "Datos del usuario", layout= "wide")
 
-st.text_input("Nombre(s)") # Para insertar texto
+Nombre = st.text_input("Nombre(s)") # Para insertar texto
+st.write("Tu nombre(s) es: ", Nombre)
 
-st.text_input("Apellido(s)")
+Apellido = st.text_input("Apellido(s)")
+st.write("Tu apellido(s) es: ", Apellido)
 
-st.text_input("Número de identificación oficial")
+N_oficial = st.text_input("Número de identificación oficial")
+st.write("Tu Número de identificación oficial es: ", N_oficial)
 
-st.text_input("Dirección actual")
+Direccion = st.text_input("Dirección actual")
+st.write("Tu Dirección actual es: ", Direccion)
 
 # Preguntas de opción múltiple
 
@@ -16,32 +20,38 @@ ObjetivoPrincipal = st.radio("¿Su objetivo principal de inversión es a ?",
                              ["Corto plazo (1-3 años)", "Mediano plazo (3-5 años)" , "Largo plazo (más de 5 años)"], 
                              index = None, horizontal = True,
 )
-st.write("Tu objetivo principal es:", ObjetivoPrincipal)
+st.write("Tu objetivo principal es: ", ObjetivoPrincipal)
 
 Riesgo = st.radio("¿Qué nivel de riesgo estás dispuesto a asumir en sus inversiones?",
                    ["Bajo", "Moderado", "Alto"], 
                    index = None, horizontal = True,
 )
-st.write("El nivel de riesgo que seleccionaste fue:", Riesgo)
+st.write("El nivel de riesgo que seleccionaste fue: ", Riesgo)
 
-mensual = st.number_input("¿Cuánto es su ingreso mensual?")
-st.number_input("¿Cuánto dinero planea invertir mensualmente sin afectar su calidad de vida actual?")
+IngMensual = st.number_input("¿Cuánto es su ingreso mensual?")
+st.write("Tu ingreso mensual es de: ", IngMensual)
 
-st.number_input("¿Cuánto es su ingreso anual?")
-st.number_input("¿Cuánto dinero planea invertir anualmente sin afectar su calidad de vida actual?")
+InvMensual = st.number_input("¿Cuánto dinero planea invertir mensualmente sin afectar su calidad de vida actual?")
+st.write("Tu inversión mensual es de: ", IngMensual)
 
+IngAnual = st.number_input("¿Cuánto es su ingreso anual?")
+st.write("Tu ingreso mensual es de: ", IngAnual)
+
+InvAnual = st.number_input("¿Cuánto dinero planea invertir anualmente sin afectar su calidad de vida actual?")
+st.write("Tu ingreso anual es de: ", InvAnual)
 
 # Preguntas de varias opciones
 
 metaInversion = st.multiselect("¿Cuáles son tus metas de inversión?", ["Casa", "Coche", "Retiro", "Educación de sus hijos", "Vacaciones", "Deudas", "Fondos de emergencia", "Otro"])
+st.write("Seleccionaste como metas: ", metaInversion)
 if "Otro" in metaInversion:
-    st.text_input('Ingresa los otros')
+    OtroMeta = st.text_input('Ingresa los otros')
+    st.write("En otros pusiste: ", OtroMeta)
 
 # Compras
 
-st.number_input("¿Cuánto gastas en necesidades (comida, servicios básico, agua, entre otros) al mes?")
+Necesidades = st.number_input("¿Cuánto gastas en necesidades (comida, servicios básico, agua, entre otros) al mes?")
+st.write("Esto es lo que gastas en necesidades: ", Necesidades)
 
-st.number_input("¿Cuánto gastas en gustos (compras de impulso, innecesarios, vicios, entre otros) al mes?")
-
-if(st.button("Continuar")):
-    st.write(mensual)
+Gustos = st.number_input("¿Cuánto gastas en gustos (compras de impulso, innecesarios, vicios, entre otros) al mes?")
+st.write("Esto es lo que gastas en gustos: ", Gustos)
