a43175f1a7c502688b1b4ca2ae807b81462dce3e
Gcumplido59 <143289394+Gcumplido59@users.noreply.github.com>
2023-09-23 23:24:22 -0600

    dataframe

---
+++ DataBase.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+d = pd.read_excel("C:\Users\gaelm\Desktop")
+df = pd.DataFrame(d)
+print(df)
+
+"""IDINE = ["C13"]
+username = ["Mario"]
+lastname = ["Feng Wu"]
+useradress = ["Tec de Monterrey"]
+useropen = ["13,000"]
+usertime =["3"]
+userisk = ["Low"]
+
+df = pd.DataFrame()
+
+df["ID"] = IDINE
+df["Username"] =  username
+df["Lastname"] = lastname
+df["Useradress"] = useradress
+df["UserOpen"] = useropen
+df["UserTime"] = usertime
+df["Userisk"] = userisk
+
+print(df)
+
+df.to_excel("C:\\Users\\gaelm\\Desktop\\Data.xlsx")"""
+
+#with pd.ExcelWriter("Output.xlsx") as writer:
+#df.to_excel(writer, sheet_name= "Prueba")
