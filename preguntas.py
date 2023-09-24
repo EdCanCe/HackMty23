d2a7039f74c42a65dbef556eebec7a4a0afa0104
takatakata195 <88916037+takatakata195@users.noreply.github.com>
2023-09-23 23:19:40 -0600

    a

--- preguntas.py
+++ preguntas.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title= "Datos del usuario", layout= "wide")
 
-st.text_input("Nombre(s)")
+st.text_input("Nombre(s)") # Para insertar texto
 
 st.text_input("Apellido(s)")
 
@@ -10,10 +10,18 @@ st.text_input("Número de identificación oficial")
 
 st.text_input("Dirección actual")
 
-st.number_input("¿Cuánto dinero ganas?")
+st.number_input("¿Cuánto dinero ganas?") # Para insertar numero
 
-st.radio('Pick one:', ['nose','ear'])
+# Preguntas de opción múltiple
+st.radio("¿Su objetivo principal de inversión es a ?", ["Corto plazo (1-3 años)", "Mediano plazo (3-5 años)" , "Largo plazo (más de 5 años)"])
 
-st.radio("¿Su objetivo principal de inversión es a ?", ["corto plazo (1-3 años)", "mediano plazo (3-5 años)" , "largo plazo (más de 5 años)"])
+st.radio("¿Qué nivel de riesgo estás dispuesto a asumir en sus inversiones?", ["Bajo", "Moderado", "Alto"])
 
-st.radio("¿Qué nivel de riesgo estás dispuesto a asumir en sus inversiones?", ["bajo", "moderado", "alto"])
+st.number_input("¿Cuánto es su ingreso mensual"?)
+st.number_input("¿Cuánto dinero planea invertir mensualmente sin afectar su calidad de vida actual?")
+
+st.number_input("¿Cuánto es su ingreso mensual"?)
+st.number_input("¿Cuánto dinero planea invertir anualmente sin afectar su calidad de vida actual?")
+
+# Preguntas de varias opciones
+print(st.multiselect("¿Cuáles son tus metas de inversión", ["Casa", "Coche", "Retiro", "Educación de sus hijos", "Vacaciones", "Otro",]))
